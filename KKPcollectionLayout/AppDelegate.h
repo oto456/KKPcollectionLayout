e75aa8c42ec80fd6d9c5c744bd0a54879882fc02
刘特风 <liutefeng@liutefengdeMacBook-Air.local>
2015-03-02 16:06:37 +0800

    Initial Commit

---
+++ KKPcollectionLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KKPcollectionLayout
+//
+//  Created by 刘特风 on 15/3/2.
+//  Copyright (c) 2015年 kakapo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KKPcollectionLayout/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KKPcollectionLayout
+//
+//  Created by 刘特风 on 15/3/2.
+//  Copyright (c) 2015年 kakapo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
